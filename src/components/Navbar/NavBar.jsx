32c72e5cf0b21015b6c53c64d46a884de0ff6124
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2024-08-12 13:11:57 +0530

    structure

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className=' bg-slate-600'>
+    <Box className="sticky top-0 w-full bg-[#c06565] z-50">
+    </Box>
+    </div>
+
+  )
+}
+
+export default NavBar
