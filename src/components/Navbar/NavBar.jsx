8623840de002b8852ac6f05eff9887b241fe5a7a
LahiruWanigasooriya <lahirunisaluor123@gmail.com>
2024-08-14 16:38:19 +0530

    navbar completed

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css'; 
 import { assets } from '../../assets/assets.js';
 
 const NavBar = () => {
+
+  const[menu,setMenu]=useState("home");
+  
   return (
     <div className='navbar'>
       <img src={assets.logo_black} alt="logo" className="logo" />
       <ul className='navbar-menu'>
-        <li>Home</li>
-        <li>Catagory</li>
-        <li>Mobile App</li>
-        <li>About Us</li>
+        <li onClick={()=>setMenu("home")}className={menu=="home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("catagory")}className={menu=="catagory"?"active":""}>Catagory</li>
+        <li onClick={()=>setMenu("mobile-app")}className={menu=="mobile-app"?"active":""}>Mobile App</li>
+        <li onClick={()=>setMenu("about-us")}className={menu=="about-us"?"active":""}>About Us</li>
       </ul>
 
       <div className='navbar-right'>
