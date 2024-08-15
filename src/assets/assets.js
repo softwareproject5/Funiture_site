08a8d44d0283458158eb92edd0de63f3a6bb4af8
LahiruWanigasooriya <lahirunisaluor123@gmail.com>
2024-08-15 18:46:53 +0530

    Login With Google Created

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,12 +2,14 @@ import cart from './cart.png'
 import logo_black from './logo_black.png'
 import search from './search.png'
 import close from './close.png'
+import google from './google.png'
 
 export const assets = {
     cart,
     logo_black,
     search,
-    close
+    close,
+    google
 }
 
 
--- src/components/LoginPopUp/UserLoginPopUp.jsx
+++ src/components/LoginPopUp/UserLoginPopUp.jsx
@@ -15,17 +15,18 @@ const UserLoginPopUp = ({ setShowLogin }) => {
                 <div className="login-popup-inputs">
                     {currentState === "LOG IN" || currentState === "ADMIN LOGIN" ? <></> : <input type="text" placeholder='Your name' required />}
 
-                    <input type="email" placeholder='Your email' required />
+                    <input type="email" placeholder='Your username or email' required />
                     <input type="password" placeholder='Password' required />
                 </div>
-                <button>{currentState === "Sign Up" ? "Create Account" : "Log In"}</button>
+                <button id='button1'>{currentState === "Sign Up" ? "Create Account" : "Log In"}</button>
+                <button id='button2'>{currentState !== "Sign Up" ? "Log In With Google" : "Sign Up With Google"}<img onClick={() => setShowLogin(false)} src={assets.google} alt="" /></button>
 
                 <div className="login-popup-condition">
                     <input type="checkbox" required />
                     <p>By continuing, I agree to the terms of use & privacy policy.</p>
                 </div>
                 {currentState !== "ADMIN LOGIN" && (currentState === "LOG IN" ? <p>Create a new account? <span onClick={() => setCurrentState("Sign Up")}>CLICK HERE</span></p> : <p>Already have an account?<span onClick={() => setCurrentState("LOG IN")}> LOGIN HERE</span></p>)}
-                {currentState === "LOG IN" ? <p>Login As an Admin <span onClick={() => setCurrentState("ADMIN LOGIN")}>CLICK HERE</span></p> : <></>}
+                {currentState === "LOG IN" ? <a>Login As an Admin <span onClick={() => setCurrentState("ADMIN LOGIN")}>CLICK HERE</span></a> : <></>}
 
             </form>
         </div>
